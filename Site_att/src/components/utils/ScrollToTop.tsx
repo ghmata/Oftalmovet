f14783396dfddac42d070ad4cd7d1635a578d7b2
GABRIEL <gabrielhm00@gmail.com>
2026-01-05 09:20:59 -0300

    fix: adiciona ScrollToTop para corrigir navegação que abria páginas no final

---
+++ Site_att/src/components/utils/ScrollToTop.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+/**
+ * Componente que faz scroll para o topo da página
+ * sempre que a rota muda
+ */
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+};
+
+export default ScrollToTop;
